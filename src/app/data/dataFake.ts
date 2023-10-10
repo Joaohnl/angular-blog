b944363c610611e13e68a536e79a80c094d70307
João Loureiro <joaohnl@gmail.com>
2023-10-10 00:35:00 -0300

    Add dataFake class
    Added the app/data/dataFake.ts class to represent data in a database

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,41 @@
+export const dataFakeBigCards = [
+	{
+		"id": 1,
+		"photo": "http://via.placeholder.com/480x300",
+		"title": "Lorem Ipsum",
+		"description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatem quia, vitae atque libero eaque consequuntur. Facere voluptate repudiandae vel voluptas, reprehenderit fuga quasi quod iusto officia quae amet magnam."
+	},
+	{
+		"id": 2,
+		"photo": "http://via.placeholder.com/480x300",
+		"title": "Lorem Ipsum",
+		"description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatem quia, vitae atque libero eaque consequuntur. Facere voluptate repudiandae vel voluptas, reprehenderit fuga quasi quod iusto officia quae amet magnam."
+	}
+]
+
+export const dataFakeSmallCards = [
+	{
+		"id": 1,
+		"photo": "http://via.placeholder.com/280x200",
+		"title": "Lorem Ipsum",
+		"description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatem quia, vitae atque libero eaque consequuntur. Facere voluptate repudiandae vel voluptas, reprehenderit fuga quasi quod iusto officia quae amet magnam."
+	},
+	{
+		"id": 1,
+		"photo": "http://via.placeholder.com/280x200",
+		"title": "Lorem Ipsum",
+		"description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatem quia, vitae atque libero eaque consequuntur. Facere voluptate repudiandae vel voluptas, reprehenderit fuga quasi quod iusto officia quae amet magnam."
+	},
+	{
+		"id": 1,
+		"photo": "http://via.placeholder.com/280x200",
+		"title": "Lorem Ipsum",
+		"description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatem quia, vitae atque libero eaque consequuntur. Facere voluptate repudiandae vel voluptas, reprehenderit fuga quasi quod iusto officia quae amet magnam."
+	},
+	{
+		"id": 1,
+		"photo": "http://via.placeholder.com/280x200",
+		"title": "Lorem Ipsum",
+		"description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatem quia, vitae atque libero eaque consequuntur. Facere voluptate repudiandae vel voluptas, reprehenderit fuga quasi quod iusto officia quae amet magnam."
+	}
+]
